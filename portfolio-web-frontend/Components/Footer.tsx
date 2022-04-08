87a4b3c41a101f4c62fd9db99aef26e4fca8a4a3
xandrf <xandrf@xandrfdev.com>
2022-04-08 15:43:04 -0300

    got a navbar from material ui, customizing it

---
+++ portfolio-web-frontend/Components/Footer.tsx
@@ -0,0 +1,10 @@
+import { faHeart } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {motion} from "framer-motion"
+export default function Footer() {
+    return (
+        <motion.div className="text-center">
+            Made with love by xandrf <FontAwesomeIcon icon={faHeart} color="red" width={15} height={15} className="m-1 align-bottom" />
+        </motion.div>
+    )
+}
--- portfolio-web-frontend/pages/_document.tsx
+++ portfolio-web-frontend/pages/_document.tsx
@@ -1,7 +1,18 @@
+import { Main, Head, NextScript, Html } from "next/document";
+import React from "react";
+import Footer from "../Components/Footer";
 import Navbar from "../Components/Navbar";
 
 export default function _document() {
     return(
-        <Navbar/>
+        <React.Fragment>
+            <Html >
+                <Head />
+                <NextScript />
+                <Navbar/>
+                <Main />
+                <Footer/>
+            </Html>
+        </React.Fragment>
     )
 }
