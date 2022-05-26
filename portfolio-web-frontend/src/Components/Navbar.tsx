7a72093e0b03999676e51249716fe0352696b5cc
xandrf <xandrf@xandrfdev.com>
2022-05-26 14:44:23 -0300

    removed mui dependencies

--- portfolio-web-frontend/src/Components/Navbar.tsx
+++ portfolio-web-frontend/src/Components/Navbar.tsx
@@ -8,29 +8,11 @@ const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 const Navbar = () => {
     let [context,SetContext] =useContext(ThemeContext)
     const [icon,SetIcon] = useState(GetIcon(context))
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
-  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
-  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElUser(event.currentTarget);
-  };
 
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-  
   useEffect(()=>{
-        console.log("getting initial icon")
         SetIcon(GetIcon(context))
     },[])
     useEffect(()=>{
-        console.log("getting update icon")
         SetIcon(GetIcon(context))
     },[context])
   return (
