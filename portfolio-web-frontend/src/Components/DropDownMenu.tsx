7e08ebb57285bc9e12888405629ca9c931ce1ba7
xandrf <xandrf@xandrfdev.com>
2022-05-26 18:33:20 -0300

    updated DropDownmenu to include state changer

--- portfolio-web-frontend/src/Components/DropDownMenu.tsx
+++ portfolio-web-frontend/src/Components/DropDownMenu.tsx
@@ -1,11 +1,13 @@
 import "./Styles/DropDownMenu.css"
+import ColorSquare from "./ColorSquare"
+import { Theme } from "../utils/ThemeContext"
 export default function DropDownMenu() {
     return(
         <ul className="DropDownMenu">
-            <li>a</li>
-            <li>b</li>
-            <li>c</li>
-            <li>d</li>
+            <li><ColorSquare Color="#171A21" Theme={Theme.Base}/></li>
+            <li><ColorSquare Color="#3C1361" Theme={Theme.HighTechPurple}/></li>
+            <li><ColorSquare Color="#FF0000" Theme={Theme.HighTechRed}/></li>
+            <li><ColorSquare Color="#229867" Theme={Theme.KissLand}/></li>
 
         </ul>
     )
