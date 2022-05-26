aa30331d54c0003a5cf345d21174bab746d9aaf6
xandrf <xandrf@xandrfdev.com>
2022-05-26 16:46:21 -0300

    updated app.tsx to include header and footer

--- portfolio-web-frontend/src/App.tsx
+++ portfolio-web-frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './style/App.css';
 import { Theme, ThemeContextProvider } from './utils/ThemeContext';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
