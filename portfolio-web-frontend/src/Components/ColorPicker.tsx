a5fe657ae8b844e7f79e5b7431d252660df1d9b9
xandrf <xandrf@xandrfdev.com>
2022-05-26 17:46:58 -0300

    added ColorPicker.tsx

--- portfolio-web-frontend/src/Components/ColorPicker.tsx
+++ portfolio-web-frontend/src/Components/ColorPicker.tsx
@@ -1,10 +1,15 @@
 import { faPalette } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import DropDownMenu from "./DropDownMenu"
 import "./Styles/ColorPicker.css"
 export default function ColorPicker() {
     return (
-        <div className="ColorPicker">
-            <FontAwesomeIcon icon={faPalette} />
-        </div>
+        <>
+            <div className="ColorPicker">
+                <FontAwesomeIcon icon={faPalette} />
+                
+            </div>
+        </>
+
     )
 }
