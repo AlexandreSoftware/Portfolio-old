ba4e14328161d09a386c9d133afe98908f009f75
xandrf <xandrf@xandrfdev.com>
2022-05-26 17:47:28 -0300

    added DropDownMenu.tsx

---
+++ portfolio-web-frontend/src/Components/DropDownMenu.tsx
@@ -0,0 +1,12 @@
+import "./Styles/DropDownMenu.css"
+export default function DropDownMenu() {
+    return(
+        <ul className="DropDownMenu">
+            <li>a</li>
+            <li>b</li>
+            <li>c</li>
+            <li>d</li>
+
+        </ul>
+    )
+}
