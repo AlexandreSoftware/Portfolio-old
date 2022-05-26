db97ac7222b8aabd07a8651c4650d26e7790b619
xandrf <xandrf@xandrfdev.com>
2022-05-26 18:34:04 -0300

    updated ThemeContext to update LocalStorage whenever state changes

--- portfolio-web-frontend/src/utils/ThemeContext.tsx
+++ portfolio-web-frontend/src/utils/ThemeContext.tsx
@@ -3,7 +3,8 @@ import React, { useEffect, useState } from "react";
 export enum Theme{
     Base,
     HighTechRed,
-    HighTechPurple
+    HighTechPurple,
+    KissLand
 }
 const ThemeContext = React.createContext<[Theme,React.Dispatch<React.SetStateAction<Theme>>]>
 ([Theme.Base, () => {}])
@@ -16,11 +17,11 @@ interface ThemeContextProps{
 
 export const ThemeContextProvider = (props : ThemeContextProps)=>{
   let themeIndex:Theme = Theme.Base;
+  
   if(typeof window !== 'undefined' &&localStorage){
     let storedTheme =localStorage.getItem("theme")
     themeIndex = (storedTheme== undefined ? Theme.Base : Theme[storedTheme as keyof typeof Theme]) 
     localStorage.setItem("theme",storedTheme== undefined ? "Base" : storedTheme)
-    
   }
   const [theme, setTheme] = useState<Theme>(themeIndex)
   return (<ThemeContext.Provider value={[theme, setTheme]}>{props.children}</ThemeContext.Provider>);
