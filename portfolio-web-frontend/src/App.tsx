4a893b0cf21e298e73e1afa7acb04161e0efbf46
xandrf <xandrf@xandrfdev.com>
2022-06-02 20:41:57 -0300

    added pages and put the router on app.tsx

--- portfolio-web-frontend/src/App.tsx
+++ portfolio-web-frontend/src/App.tsx
@@ -1,17 +1,31 @@
 import React, { useContext } from 'react';
-import logo from './logo.svg';
 import './Styles/App.css';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
 import ThemeContext, { Theme } from './utils/ThemeContext';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home';
+import Projects from './Pages/Projects';
+import About from './Pages/About';
+import Skills from './Pages/Skills';
+import Blog from './Pages/Blog';
 
 function App() {
-  let [context,SetContext] =useContext(ThemeContext)
+  let [context] =useContext(ThemeContext)
   return (
     <div className={`App App-${Theme[context]}`}>
-      <Navbar/>   
-          
-      <Footer/>
+      <BrowserRouter>
+        <Navbar/>   
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/Projects" element={<Projects/>}/>
+          <Route path="/Blog" element={<Blog/>}/>
+          <Route path="/Skills" element={<Skills/>}/>
+          <Route path="/About" element={<About/>}/>
+
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
     </div>
   );
 }
