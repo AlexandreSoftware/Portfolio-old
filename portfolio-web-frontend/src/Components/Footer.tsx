b83be0581f3e94bbeddfccd8016690623f08c987
xandrf <xandrf@xandrfdev.com>
2022-06-02 19:56:53 -0300

    changed footer so it auto goes to the bottom of the screen

--- portfolio-web-frontend/src/Components/Footer.tsx
+++ portfolio-web-frontend/src/Components/Footer.tsx
@@ -1,8 +1,9 @@
 import { faHeart } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import "./Styles/Footer.css"
 export default function Footer() {
     return (
-        <footer className="text-center">
+        <footer className="text-center footer">
             Made with love by xandrf <FontAwesomeIcon icon={faHeart} color="red" width={15} height={15} className="m-1 align-bottom" />
         </footer>
     )
