c68317bff87c19061964ba5b73c60254c0d6c68f
xandrf <xandrf@xandrfdev.com>
2022-06-10 19:05:44 -0300

    i think the action is working now

--- portfolio-web-frontend/next.config.js
+++ portfolio-web-frontend/next.config.js
@@ -5,6 +5,7 @@ const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
   reactStrictMode: true,
 }
+
 module.exports ={
   ...nextConfig,
   assetPrefix: isProd ? 'https://cdn.statically.io/gh/xandrf/xandrf.github.io/gh-pages/' : ''
