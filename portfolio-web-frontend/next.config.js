e72bf5327674bcab9419badb8d6eccccd6909570
xandrf <xandrf@xandrfdev.com>
2022-06-10 17:25:39 -0300

    added updated next config

--- portfolio-web-frontend/next.config.js
+++ portfolio-web-frontend/next.config.js
@@ -4,5 +4,7 @@ const optimizedImages = require('next-optimized-images');
 const nextConfig = {
   reactStrictMode: true,
 }
-module.exports =
-  nextConfig;
+module.exports ={
+  nextConfig,
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/xandrf/xandrf.github.io/gh-pages/' : ''
+}
