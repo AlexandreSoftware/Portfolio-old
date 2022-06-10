9b80d536c404310efb40946a070cbdf51819e60e
xandrf <xandrf@xandrfdev.com>
2022-06-10 20:36:10 -0300

    trying the other config on next to see if it deploys correctly

--- portfolio-web-frontend/next.config.js
+++ portfolio-web-frontend/next.config.js
@@ -1,13 +1,19 @@
 /** @type {import('next').NextConfig} */
+const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
-const isProd = process.env.NODE_ENV === 'production'
-
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports ={
-  ...nextConfig,
-  assetPrefix: '/',
-  assetPrefix: isProd ? 'https://cdn.statically.io/gh/xandrf/xandrf.github.io/gh-pages/' : ''
-}
+module.exports = withPlugins([
+  [optimizedImages, {
+    mozjpeg: {
+      quality: 80,
+    },
+    pngquant: {
+      speed: 3,
+      strip: true,
+      verbose: true,
+    },
+  }],
+  {
+    
+    reactStrictMode: true
+  },
+]);
