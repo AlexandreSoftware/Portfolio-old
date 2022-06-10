b941962c9ebf1548297e8f3bba12be6c9c6d09cb
xandrf <xandrf@xandrfdev.com>
2022-06-10 17:37:18 -0300

    changed error in next.config.js

--- portfolio-web-frontend/next.config.js
+++ portfolio-web-frontend/next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const optimizedImages = require('next-optimized-images');
+const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
   reactStrictMode: true,
 }
 module.exports ={
-  nextConfig,
+  ...nextConfig,
   assetPrefix: isProd ? 'https://cdn.statically.io/gh/xandrf/xandrf.github.io/gh-pages/' : ''
 }
