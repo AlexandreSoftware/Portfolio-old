63c217157986bd53843115a89624a0f084d82982
xandrf <xandrf@xandrfdev.com>
2022-06-14 01:09:11 -0300

    added a loading screen before the page loads

--- portfolio-web-frontend/pages/_app.tsx
+++ portfolio-web-frontend/pages/_app.tsx
@@ -7,18 +7,23 @@ import Background from '../Components/Background'
 import Navbar from '../Components/Navbar'
 import Footer from '../Components/Footer'
 import Head from 'next/head'
+import { useEffect, useState } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [isSSR, setIsSSR] = useState(true);
+  useEffect(()=>{
+    setIsSSR(false)
+  },[])
   return (
   <ThemeContextProvider>
     <OldThemeContextProvider>
-        <Head>
-          <title>Xandrf Portfolio</title>
-        </Head>
-        <Navbar/>
-        <Background/>   
-          <Component {...pageProps} />
-        <Footer/>
+      <Head>
+        <title>Xandrf Portfolio</title>
+      </Head>
+      <Navbar/>
+        {!isSSR &&<Background/>}
+        <Component {...pageProps} />
+      <Footer/>
     </OldThemeContextProvider>
   </ThemeContextProvider>
   )
---
+++ portfolio-web-frontend/pages/_document.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+// Modules
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Script from "next/script"
+export default function MyDocument() {
+
+    return (
+      <Html lang="en">
+        <Head>
+          <script src="/noflicker.js" strategy="beforeInteractive" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+}
