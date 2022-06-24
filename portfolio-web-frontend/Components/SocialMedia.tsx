12b174d7b41deadc95ae03edd9eac6e65bbab176
xandrf <xandrf@xandrfdev.com>
2022-06-24 07:02:06 -0300

    added animation on socialscard that pulls all of the socials one social at a time

--- portfolio-web-frontend/Components/SocialMedia.tsx
+++ portfolio-web-frontend/Components/SocialMedia.tsx
@@ -1,17 +1,35 @@
 import { icon, IconProp } from "@fortawesome/fontawesome-svg-core";
 import { faHeader } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { animate, motion } from "framer-motion";
 import Style from "./Styles/SocialMedia.module.css"
 
 interface SocialMediaProps{
     icon : IconProp,
     link : string,
-    color : string
+    color : string,
+    delay:number
 }
 export default function SocialMedia(props:SocialMediaProps) {
     return(
-        <a href={props.link} className={Style.MediaLink}>
+        <motion.a 
+        initial="initial"
+        animate="animate"
+        variants={{
+            initial:{
+                x:-100
+            },
+            animate:{
+                x:0,
+                transition:{
+                    delay:0.5 + props.delay,
+                    duration:0.3,
+                    ease:"linear"
+                }
+            }
+        }}
+        href={props.link} className={Style.MediaLink}>
             <FontAwesomeIcon className={Style.MediaImage}  icon={props.icon} color={props.color} />
-        </a>
+        </motion.a>
     )
 }
--- portfolio-web-frontend/Components/SocialsCard.tsx
+++ portfolio-web-frontend/Components/SocialsCard.tsx
@@ -1,16 +1,32 @@
 import { faDev, faGithub, faReddit, faTwitch, faTwitter } from "@fortawesome/free-brands-svg-icons"
+import { motion } from "framer-motion"
 import BlackLine from "./BlackLine"
 import SocialMedia from "./SocialMedia"
 import Style from "./Styles/SocialsCard.module.css"
 export default function SocialsCard() {
     return(
-        <div className={Style.SocialsCard}>
-            <SocialMedia icon={faTwitter} link="https://www.twitter.com/XandrfDev" color="#1DA1F2" ></SocialMedia>
-            <SocialMedia icon={faGithub} link="https://www.twitter.com/XandrfDev" color="white"></SocialMedia>
-            <SocialMedia icon={faReddit} link="https://www.twitter.com/XandrfDev" color="#FF5700"></SocialMedia>
-            <SocialMedia icon={faTwitch} link="https://www.twitter.com/XandrfDev" color="#6441a5"></SocialMedia>
-            <SocialMedia icon={faDev} link="https://www.twitter.com/XandrfDev" color="white"></SocialMedia>
+        <motion.div
+        initial="initial"
+        animate="animate"
+        variants={{
+            initial:{
+                y:100
+            },
+            animate:{
+                y:0,
+                transition:{
+                    duration:0.5,
+                    ease:"linear"
+                }
+            }
+        }}
+        className={Style.SocialsCard}>
+            <SocialMedia delay={1.2} icon={faTwitter} link="https://www.twitter.com/XandrfDev" color="#1DA1F2" ></SocialMedia>
+            <SocialMedia delay={0.9} icon={faGithub} link="https://www.twitter.com/XandrfDev" color="white"></SocialMedia>
+            <SocialMedia delay={0.6} icon={faReddit} link="https://www.twitter.com/XandrfDev" color="#FF5700"></SocialMedia>
+            <SocialMedia delay={0.3} icon={faTwitch} link="https://www.twitter.com/XandrfDev" color="#6441a5"></SocialMedia>
+            <SocialMedia delay={0} icon={faDev} link="https://www.twitter.com/XandrfDev" color="white"></SocialMedia>
             <BlackLine/>
-        </div>
+        </motion.div>
         )
 }
