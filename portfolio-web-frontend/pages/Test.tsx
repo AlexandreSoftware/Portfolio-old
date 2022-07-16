84252c4c4047492f69d9fe076dacf6b897f80ca6
xandrf <xandrf@xandrfdev.com>
2022-07-16 18:48:28 -0300

    working on alignedlink, fixing a merge conflict for now

--- portfolio-web-frontend/pages/Test.tsx
+++ portfolio-web-frontend/pages/Test.tsx
@@ -14,7 +14,9 @@ export default function Test() {
     
     return (
         <div className={Style.Test}>
-            {state && <BackgroundChangeBall element={state!} colorToChange={Theme.HighTechRed} reverse={false}/>}
+            <ProjectSummaryCard Title="Wordle-Clone" imageLink="/Logo WordleClone.svg"> 
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique neque volutpat neque nulla sed vel. A justo et, elit, in sit gravida in. Vel integer pellentesque magna aliquet.
+            </ProjectSummaryCard>
         </div>
     )
 
