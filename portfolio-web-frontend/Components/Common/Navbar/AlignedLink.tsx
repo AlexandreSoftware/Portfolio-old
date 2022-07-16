eedca5c150d311c78677b7a30d5aa92d8fb7ae07
xandrf <xandrf@xandrfdev.com>
2022-07-16 01:47:03 -0300

    changed alignedlink so that its not a button, and shrunk navbar

--- portfolio-web-frontend/Components/Common/Navbar/AlignedLink.tsx
+++ portfolio-web-frontend/Components/Common/Navbar/AlignedLink.tsx
@@ -12,30 +12,9 @@ export default function AlignedLink(props :AlignedLinkProps) {
     let [context,SetContext] = useContext(ThemeContext)
     return (
     <Link href={props.href}>
-        <div className={Style.AlignedLinkContainer}>
-            <motion.div className={Style.AlignedLink}
-                whileHover="hover"
-                variants={{
-                    hover:{
-                        border:"1px solid black",
-                        scale:1.2,
-                        backgroundColor:GetTheme(context),
-                        zIndex:5
-                    }
-                }}
-            ><AlignedText>{props.children}</AlignedText></motion.div>
+        <div className={`${Style.AlignedLink} ${Style[`AlignedLink-${Theme[context]}`]}`}>
+            <AlignedText>{props.children}</AlignedText>
         </div>
     </Link>
     )
-}
-
-function GetTheme(theme:Theme){
-    return (theme== Theme.Base ?
-    "#6B7FD7":
-        theme == Theme.HighTechPurple ?
-            "#F05E23"
-                :theme==Theme.HighTechRed ?
-                "#150B0E"
-                        :"#449BAA")
-
 }
