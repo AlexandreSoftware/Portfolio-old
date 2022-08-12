df891a8621200592801b98f2f5ad068a76a3fd62
xandrf <xandrf@xandrfdev.com>
2022-08-12 14:23:21 -0300

    changed socialscard to be on top

--- portfolio-web-frontend/Components/Common/SocialsCards/Loading.tsx
+++
@@ -1,9 +0,0 @@
-import Style from "./Styles/Loading.module.css"
-import ReactLoading from "react-loading";
-export default function Loading() {
-    return(
-        <div className={Style.Loading}>
-            <ReactLoading type="spin"/>
-        </div>
-    )
-}
--- portfolio-web-frontend/Components/Common/SocialsCards/SocialMedia.tsx
+++ portfolio-web-frontend/Components/Common/SocialsCards/SocialMedia.tsx
@@ -8,28 +8,12 @@ interface SocialMediaProps{
     icon : IconProp,
     link : string,
     color : string,
-    delay:number
 }
 export default function SocialMedia(props:SocialMediaProps) {
     return(
-        <motion.a 
-        initial="initial"
-        animate="animate"
-        variants={{
-            initial:{
-                x:-100
-            },
-            animate:{
-                x:0,
-                transition:{
-                    delay:0.5 + props.delay,
-                    duration:0.3,
-                    ease:"linear"
-                }
-            }
-        }}
+        <a 
         href={props.link} className={Style.MediaLink}>
             <FontAwesomeIcon className={Style.MediaImage}  icon={props.icon} color={props.color} />
-        </motion.a>
+        </a>
     )
 }
